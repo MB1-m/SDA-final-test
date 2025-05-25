68cde0c06a10ba7260136bb312a68db86a3b31b3
Miroslav Barta <jedi1@seznam.cz>
2025-05-25 13:41:22 +0200

    new

--- src/main/java/demotest/SDATest.java
+++ src/main/java/demotest/SDATest.java
@@ -1,16 +1,30 @@
 package demotest;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SDATest {
 
     private WebDriver driver;
     @Before
  public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Disk D\\SDA kurzy\\Java\\SADFinalT1est\\src\\main\\resources\\chromedriver.exe");
+driver = new ChromeDriver();
 
 
     }
+
+    @Test
+    public void sdaTest(){
+        driver.get("https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php");
+        // toto mi najdede na str8nce prvek a napise jmeno
+        driver.findElement(By.id("name")).sendKeys("Mir");
+        driver.findElement(By.id("email")).sendKeys("jedi1");
+    }
+
 
 
 }
