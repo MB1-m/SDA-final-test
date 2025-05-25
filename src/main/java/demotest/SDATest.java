bbeea9c466987795321dcc50a056d9600847fe93
Miroslav Barta <jedi1@seznam.cz>
2025-05-25 13:27:04 +0200

    new

--- src/main/java/demotest/SDATest.java
+++ src/main/java/demotest/SDATest.java
@@ -1,4 +1,16 @@
 package demotest;
 
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
 public class SDATest {
+
+    private WebDriver driver;
+    @Before
+ public void setUp(){
+
+
+    }
+
+
 }
